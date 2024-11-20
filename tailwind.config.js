1b98c1665dd009ea5b1df2fde1d150a098b5088a
NGUYENQUANGHUY1106 <nguyenquanghuy110605@gmail.com>
2024-11-20 15:14:44 +0700

    Ecomeorce

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,25 @@ module.exports = {
       {
         ebebe9:'#EBEBE9'
       },
+      keyframes:
+      {
+        slideInleft:
+        {
+          '0%':
+          {
+            opacity :"0",
+            transform :"translateX(-150px)"
+          },
+          '100%':
+          {
+             transform: "translateX(0)"
+          }
+        }
+      },
+      animation:
+      {
+        slideInleft: "slideInleft .5s ease-out backwards"
+      }
     },
   },
   plugins: [],
